aff8b82a24d2a9e301d178f6ed78a57e85ba0d5e
Zixuan Ge <zixuan.ge@berkeley.edu>
2016-12-27 22:27:51 +0800

    finished

--- js/main.js
+++ js/main.js
@@ -1,32 +1,86 @@
-// var currentquote='';
-// var currentauthor='';
-// function opentwitterURL(url){
-//   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-// }
-// function getQuote(){
-//   $.ajax({
-//     headers: {
-//   "X-Mashape-Key": "PyOKfsU1hnmsh28BEUIty7Wl6MEIp1fbQypjsnH0nOMD1r3moP",
-//   Accept : "application/json",
-//     "Content-Type": "application/x-www-form-urlencoded"
-//     },
-//     url:'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-//     success: function(response) {
-//       var r = JSON.parse(response);
-//       currentquote = r.quote;
-//       currentauthor = r.author;
-//        $(".qtext").animate({}, 1000,
-//         function() {
-//   $(this).animate({opacity:1},1000);
-//   $('#text').text(r.quote);
-//         });
+// function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
 
-//       $(".qauthor").animate({}, 1000,
-//         function() {
-//           $(this).animate({}, 1000);
-//          $('#author').html(r.author);
-//         });}});}
-// $(document).ready(function() {
-//   getQuote();
-//   $('#quote').on('click', getQuote);
-//  $('#tweet').on('click', function() { opentwitterURL('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + currentquote + '" ' + currentauthor));});});
+var currentQuote = ''; 
+var currentAuthor = '';
+// var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+// function changecolor() {
+// 	var color = Math.floor(Math.random() * colors.length);
+// 	  $(".textbox").animate({
+//         backgroundColor: colors[color]
+//       }, 1000);
+// }
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "RElYuHifQfmshiuEjqQVlRFMVEKsp1B4CZMjsnnodZVCkhf8sO",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous',
+    success: function(res) {
+      var r = JSON.parse(res);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      
+
+      $(".quote-text").animate({
+          opacity: 0
+        }, 1000,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#text').text(r.quote);
+        });
+
+      $(".quote-author").animate({
+          opacity: 0
+        }, 1000,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').html(r.author);
+        });
+
+      if ((currentQuote.length + currentAuthor.length + 1) > 140){
+      	$("#tweet").hide();
+	  } else {
+	  	$("#tweet").show();
+	  }
+
+	  // var color = Math.floor(Math.random() * colors.length);
+   //    $("html body").animate({
+   //      backgroundColor: colors[color]
+   //    }, 1000);
+   //    $(".textbox").animate({
+   //      backgroundColor: colors[color]
+   //    }, 1000);
+
+    }
+  });
+}
+
+
+$(document).ready(function() {
+  getQuote();
+  $('#quoteButton').on('click', getQuote);
+  $('#tweet').on('click', function() {
+  openURL('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));    
+  });
+  $('#fb').on('click', function() {
+  openURL('http://www.facebook.com/sharer');
+  });
+  $('#tumblr').on('click', function() {
+  openURL('https://www.tumblr.com/share');
+  });
+});
+
+
+
